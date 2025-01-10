2a70f5e902a4c58d5c920419098cbdefec254763
Oleg Vovchenko <o.vovchenko91@gmail.com>
2025-01-10 23:29:57 +0300

    hw21: Создание python_blog.urls и python_blog.views

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,9 @@
+from django.contrib import admin
 from django.urls import path, include
+from python_blog.views import main
 
 urlpatterns = [
-    path('posts/', include('python_blog.urls'))
+    path('admin/', admin.site.urls),
+    path('', main),
+    path('posts/', include('python_blog.urls')),
 ]
--- python_blog/urls.py
+++ python_blog/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
+from python_blog.views import catalog_posts, post_detail, catalog_categories, category_detail, catalog_tags, tag_detail
 
 urlpatterns = [
-    path('/', main, name='main'),
-    path('/posts/', catalog_posts, name='catalog_posts'),
-    path('/posts/categories/', catalog_categories, name='catalog_categories'),
-    path('/posts/categories/<slug:category_slug>/', category_detail, name='category_detail'),
-    path('/posts/tags/', catalog_tags, name='catalog_tags'),
-    path('/posts/tags/<slug:tag_slug>/', tag_detail, name='tag_detail'),
-    path('/posts/<slug:post_slug>/', post_detail, name='post_detail'),
+    path('', catalog_posts, name='catalog_posts'),
+    path('<slug:post_slug>/', post_detail, name='post_detail'),
+
+    # Категории
+    path('categories/', catalog_categories, name='catalog_categories'),
+    path('categories/<slug:category_slug>/', category_detail, name='category_detail'),
+    
+    # Теги
+    path('tags/', catalog_tags, name='catalog_tags'),
+    path('tags/<slug:tag_slug>/', tag_detail, name='tag_detail'),
 ]
