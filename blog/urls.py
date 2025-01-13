67c228041530aa4f92a8807ca7e34965d6c9f9ef
Oleg Vovchenko <o.vovchenko91@gmail.com>
2025-01-13 18:35:25 +0300

    hw22: Создали статик, настроили его и добавили в базовый шаблон

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from python_blog.views import main
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main, name='main'),
+
+    # Подключаем python_blog.urls
     path('posts/', include('python_blog.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
