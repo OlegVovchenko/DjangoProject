37a1c2c93a97462acadf63de9e7dc6f262c8d76b
Oleg Vovchenko <o.vovchenko91@gmail.com>
2025-01-26 08:57:26 +0300

    hw23: обновленное поле тегов, связь Многие ко многим

--- python_blog/models.py
+++ python_blog/models.py
@@ -1,38 +1,63 @@
+from django import views
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
 from unidecode import unidecode
+# Функция get_userr_model() возвращает модель пользователя, которая используется по умолчанию в проекте.
+from django.contrib.auth import get_user_model
+
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100, unique=True, verbose_name="Заголовок")
+    slug = models.SlugField(max_length=250, unique=True, verbose_name="Слаг", blank=True, null=True)
     content = models.TextField(verbose_name="Контент")
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name="Автор", related_name="posts", default=None, null=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+    views = models.PositiveIntegerField(default=0, verbose_name="Количество просмотров")
     # Категория - внешний ключ
     category = models.ForeignKey(
-        "Category", # Ссылка на модель категории
+        "Category", # Ссылка на модель Category
         on_delete=models.SET_NULL, # При удалении категории, установить значение NULL
         blank=True, # Не требуем в формах заполнения
         null=True, # Разрешаем значение NULL в базе данных
         related_name="posts", # Имя обратной связи
-        default=None # Значение по умолчанию NULL
+        default=None, # Значение по умолчанию NULL
+        verbose_name="Категория",
     )
+    tags = models.ManyToManyField("Tag", related_name="posts", blank=True, verbose_name="Теги")
 
     def __str__(self):
         return self.title
     
     def get_absolute_url(self):
-        """
-        Метод возвращает абсолютный URL для объекта Post
-        В админке Django, при создании или редактировании поста, будет ссылка "Посмотреть на сайте". В шаблонах тоже удобно вызвать его.
-        """
         return reverse("blog:post_detail", args=[self.slug])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(unidecode(self.title))
+        super().save(*args, **kwargs)
     
     class Meta:
         verbose_name = "Пост"
         verbose_name_plural = "Посты"
         ordering = ["-created_at"]
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Название")
+    slug = models.SlugField(max_length=250, unique=True, verbose_name="Слаг")
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self , *args, **kwargs):
+        self.slug = slugify(unidecode(self.name))
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
+        ordering = ["name"]
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name="Название")
