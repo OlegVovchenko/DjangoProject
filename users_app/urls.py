25e8f25c332f5a611c2b870f7fb94f3ae81e16a8
Oleg Vovchenko <o.vovchenko91@gmail.com>
2025-03-01 20:02:06 +0300

    hw27: подключение приложения в settings.py, добавление кнопок в входа/выхода/регистрации в меню, создание основных маршрутов в users_app

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,1 +1,13 @@
+from django.urls import path
+from users_app.views import login_user, CustomLogoutView, register_user, profile_user
+
 app_name = 'users'
+
+urlpatterns = [
+    path('login/', login_user, name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+    path('register/', register_user, name='register'),
+    path('profile/', profile_user, name='profile'),
+]
+
+ 
