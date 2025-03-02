64d3c6870d0c63dcf18895b5a92733ba355fe446
Oleg Vovchenko <o.vovchenko91@gmail.com>
2025-03-02 08:46:29 +0300

    hw27: вывод сообщений об ошибке на руссом языке в теле формы

--- users_app/forms.py
+++ users_app/forms.py
@@ -2,6 +2,10 @@ from django.contrib.auth.forms import AuthenticationForm
 from django import forms
 
 class LoginForm(AuthenticationForm):
+    error_messages = {
+        'invalid_login': 'Неверное имя пользователя или пароль',
+        'inactive': 'Этот аккаунт неактивен',
+    }
     username = forms.CharField(
         label="Имя пользователя",
         widget=forms.TextInput(attrs={
